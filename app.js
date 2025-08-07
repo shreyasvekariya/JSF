84f8b373cea8c6b12df3ee4bd5e60344bc427615
shreyasvekariya <140139916+shreyasvekariya@users.noreply.github.com>
2025-08-07 12:55:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+// CREATE DATABASE IF NOT EXISTS test;
+// USE test;
+
+// CREATE TABLE users (
+//   id INT AUTO_INCREMENT PRIMARY KEY,
+//   username VARCHAR(100),
+//   email VARCHAR(100),
+//   password VARCHAR(255)
+// );
+
+// CREATE TABLE contact_messages (
+//   id INT AUTO_INCREMENT PRIMARY KEY,
+//   name VARCHAR(100),
+//   email VARCHAR(100),
+//   message TEXT
+// );
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
